af2777571a3711b853276b8a2479239251ce87aa
emushtaq <eshanmushtaq@gmail.com>
2016-07-28 12:17:18 +0200

    Added time tracking and blocks

---
+++ Tris/Block.swift
@@ -0,0 +1,43 @@
+//
+//  Block.swift
+//  Tris
+//
+//  Created by Eshan on 7/28/16.
+//  Copyright © 2016 Eshan. All rights reserved.
+//
+
+import SpriteKit
+
+
+let NumberOfColors: UInt32 = 6
+
+
+enum BlockColor: Int, CustomStringConvertible {
+    
+    case Blue = 0, Orange, Purple, Red, Teal, Yellow
+    
+    var spriteName: String {
+        switch self {
+        case .Blue:
+            return "blue"
+        case .Orange:
+            return "orange"
+        case .Purple:
+            return "purple"
+        case .Red:
+            return "red"
+        case .Teal:
+            return "teal"
+        case .Yellow:
+            return "yellow"
+        }
+    }
+    
+    var description: String {
+        return self.spriteName
+    }
+    
+    static func random() -> BlockColor {
+        return BlockColor(rawValue:Int(arc4random_uniform(NumberOfColors)))!
+    }
+}
